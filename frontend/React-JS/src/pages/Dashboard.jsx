d83a0843afd7f776a1dfe801e2888d4704c704b9
Rishikeshh19 <rishikeshmane19@gmail.com>
2025-11-26 19:40:06 +0530

    Deleting all files

--- frontend/React-JS/src/pages/Dashboard.jsx
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-  return (
-    <div className="dashboard-box">
-      <h2>Welcome to Dashboard 🎉</h2>
-      <p>This is your protected area. You can display user data, charts, etc.</p>
-    </div>
-  );
-}
